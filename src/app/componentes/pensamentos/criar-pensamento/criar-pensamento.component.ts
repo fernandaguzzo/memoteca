2683736d4ff6d674104be897b891748c024b486a
Fernanda <guzzofernanda88@gmail.com>
2025-02-14 11:23:03 -0300

    criação de formulário reativo no componente de edição de pensamento  e customização de uma validação

--- src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -3,6 +3,7 @@ import { Pensamento } from '../pensamento';
 import { PensamentoService } from '../pensamento.service';
 import { Router } from '@angular/router';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { minusculoValidator } from './minusculoValidators';
 
 @Component({
   selector: 'app-criar-pensamento',
@@ -25,9 +26,10 @@ export class CriarPensamentoComponent implements OnInit {
         Validators.required,
         Validators.pattern(/(.|\s)*\s(.|\s)*/)
       ])],
-      autoria: ['Angular', Validators.compose([
+      autoria: ['', Validators.compose([
         Validators.required,
-        Validators.minLength(3)
+        Validators.minLength(3),
+        minusculoValidator
 
         ])],
       modelo: ['modelo1']
