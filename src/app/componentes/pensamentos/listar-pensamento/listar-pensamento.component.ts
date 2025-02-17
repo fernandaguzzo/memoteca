fddfbb9a53be72dc852ed3009f66b51f9453c136
Fernanda <guzzofernanda88@gmail.com>
2025-02-16 23:56:31 -0300

    campo e filtro de busca

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -14,23 +14,31 @@ export class ListarPensamentoComponent implements OnInit {
   listaPensamentos: Pensamento[] = [];
   paginaAtual = 1;
   haMaisPensamentos: boolean = true;
+  filtro: string = ''
 
   constructor(private service: PensamentoService) { }
 
   ngOnInit(): void {
     /* com o subscribe, o observable vai entender que é necessário emitir notificações toda vez que houver uma mudança */
-    this.service.listar(this.paginaAtual).subscribe((listaPensamentos) => {
+    this.service.listar(this.paginaAtual, this.filtro).subscribe((listaPensamentos) => {
       this.listaPensamentos = listaPensamentos
     })
   }
 
   carregarMaisPensamentos() {
-    this.service.listar(++this.paginaAtual).subscribe(listaPensamentos => {
+    this.service.listar(++this.paginaAtual, this.filtro).subscribe(listaPensamentos => {
       this.listaPensamentos.push(...listaPensamentos);
       if (!this.listaPensamentos.length) {
         this.haMaisPensamentos = false;
       }
     });
 }
-
+  pesquisarPensamentos() {
+    this.haMaisPensamentos = true
+    this.paginaAtual = 1; // sempre que a pessoa pesquisa, a página do pensamento é a primeira
+    this.service.listar(this.paginaAtual, this.filtro)
+      .subscribe(listaPensamentos => {
+        this.listaPensamentos = listaPensamentos
+      })
+  }
 }
